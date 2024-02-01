c31c4f78f857a4d29cf63372d768a8518dddaa5a
tushar-nb <tushar02borkade@gmail,com>
2024-02-01 17:34:07 +0530

    textLimit added

--- TaxCalculator/app/src/main/java/com/example/taxcalculator/MainActivity.kt
+++ TaxCalculator/app/src/main/java/com/example/taxcalculator/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.taxcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,8 +16,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -37,6 +42,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.taxcalculator.ui.theme.TaxCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -55,25 +62,41 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-                                                                                                                        //edit/input field
+
 @Composable
-fun EditNumberField( onValueChange: (String)->Unit,modifier: Modifier=Modifier,amountInput:String,leadingIcon:Int){
+fun editNumberField( onValueChange: (String)->Unit,modifier: Modifier=Modifier,amountInput:String,leadingIcon:Int){
 
     Column {
+        var text by remember { mutableStateOf("") }
+        val maxChar = 9
         TextField(
             label = { Text(text = "Total Income Amount:")},
             leadingIcon = { Icon(painter = painterResource(id = leadingIcon), contentDescription = null,modifier = Modifier.size(24.dp))},
-            value = amountInput,
-            onValueChange = onValueChange,
+//            value = amountInput,
+            value = text,
+//            onValueChange = onValueChange,
+            onValueChange = {
+                if(it.length<=maxChar) {
+                    text = it
+                    onValueChange(text)
+                }
+            },
             singleLine = true,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             modifier = modifier
         )   
     }
 }
-                                                                                                            //calculate tax
+
 private fun computeTax(amount:String):Double{
-    when(val income = amount.toDouble()){
+    var income:Double
+    if(amount==" "){
+         income = 0.0
+    }
+    else {
+        income = amount.toDouble()
+    }
+    when( income){
         in 0.0..300000.0 -> return 0.0
         in 300001.0..600000.0 -> {   var amt :Double=0.00+ (income-300000)
                                             amt = (0.05*amt)
@@ -97,15 +120,26 @@ private fun computeTax(amount:String):Double{
                 }
     }
 }
-                                                                                      //main default calculate function
+
+@Composable
+private fun displayTax(message:String, tax:Double) {
+    Text(
+        modifier = Modifier
+            .padding(16.dp),
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
+        text = message+ tax.toString()
+    )
+}
+
 @Composable
 fun  CalculateTax(message: String,title:String, modifier: Modifier = Modifier) {
 
     var amountInput by remember {
-        mutableStateOf("0")
+        mutableStateOf(" ")
     }
     if(amountInput=="") {
-        amountInput="0"
+        amountInput=" "
     }
     val tax = computeTax(amountInput)
 
@@ -115,9 +149,9 @@ fun  CalculateTax(message: String,title:String, modifier: Modifier = Modifier) {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 12.dp, bottom = 12.dp)
-                .fillMaxHeight(0.05f)
+                .fillMaxHeight(0.06f)
                 .background(color = Color.LightGray)
-                .verticalScroll(rememberScrollState())      //landscape page scrolling and adjusting
+                .verticalScroll(rememberScrollState())
                 .wrapContentHeight(align = Alignment.CenterVertically),
             textAlign = TextAlign.Center,
 
@@ -126,24 +160,41 @@ fun  CalculateTax(message: String,title:String, modifier: Modifier = Modifier) {
 
         )
         Spacer(modifier = Modifier.height(50.dp))
-        EditNumberField(
+        editNumberField(
             onValueChange = {amountInput = it},
             modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth(),
             amountInput,
             leadingIcon = R.drawable.income)
-        Spacer(modifier = Modifier.height(50.dp))
-        Text(
+        Spacer(modifier = Modifier.height(100.dp))
+
+        Button(
+            onClick = {/*todo*/},
+            colors = ButtonDefaults.buttonColors( Color.Blue),
+            shape = RoundedCornerShape(8.dp),
+            contentPadding = PaddingValues(16.dp),
             modifier = Modifier
-                .padding(16.dp),
-            fontWeight = FontWeight.Bold,
-            fontSize = 28.sp,
-            text = message+ tax.toString()
-        )
+                .align(Alignment.CenterHorizontally)
+
+        ) {
+            Text(text = "Calculate")
+        }
+
+        if(amountInput!=" " ) {
+            displayTax(message = message, tax =tax )
+        }
+
     }
 
+
 }
+@Composable
+fun someFunction() {
+    TODO("Not yet implemented")
+}
+
+
 
 @Preview(showBackground = true, showSystemUi = true, name="calculate tax")
 @Composable
