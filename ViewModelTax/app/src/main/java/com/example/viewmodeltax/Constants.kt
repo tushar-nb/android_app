4131f91f7eb5370950d35472890876ec4756554e
tushar-nb <tushar02borkade@gmail,com>
2024-02-16 18:30:15 +0530

    constants File

---
+++ ViewModelTax/app/src/main/java/com/example/viewmodeltax/Constants.kt
@@ -0,0 +1,28 @@
+package com.example.viewmodeltax
+
+
+const val ZERO = 0.00
+const val FIXED_AMOUNT_6 = 15000.00
+const val FIXED_AMOUNT_9 = 45000.00
+const val FIXED_AMOUNT_12 = 90000.00
+const val FIXED_AMOUNT_16 = 150000.00
+const val START_1 = 0.00
+const val END_1 = 300000.00
+const val START_2 = 30000.10
+const val END_2 = 600000.00
+const val START_3 = 600000.10
+const val END_3 = 900000.00
+const val START_4 = 900000.10
+const val END_4 = 1200000.00
+const val START_5 = 1200000.10
+const val END_5 = 1500000.00
+const val LIMIT_1 = 300000.00
+const val LIMIT_2 = 600000.00
+const val LIMIT_3 = 900000.00
+const val LIMIT_4 = 1200000.00
+const val LIMIT_5 = 1500000.00
+const val SLAB_1_PERCENT = 0.05
+const val SLAB_2_PERCENT = 0.10
+const val SLAB_3_PERCENT = 0.15
+const val SLAB_4_PERCENT = 0.20
+const val SLAB_5_PERCENT = 0.30
---
+++ ViewModelTax/app/src/test/java/com/example/viewmodeltax/TaxViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.example.viewmodeltax
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class TaxViewModelTest {
+
+    @Test
+    fun doCalculation() {
+    }
+}
